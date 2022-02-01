2d91a1228c20d81e09d26664953467a051acb914
Saw Simeon <doe_doe207@hotmail.com>
2022-02-01 11:46:56 +0100

    renamed files and added extract descriptors script

--- data/extract_descriptors.py
+++ data/extract_descriptors.py
@@ -5,14 +5,27 @@ import tempfile
 import deepchem as dc
 from deepchem.utils.vina_utils import prepare_inputs
 from deepchem.feat import RdkitGridFeaturizer
+from oddt.pandas import ChemDataFrame
+from oddt.fingerprints import PLEC
+import oddt
 
 featurizer = RdkitGridFeaturizer(voxel_width=16.0, feature_types=["ecfp", "splif", "hbond", "salt_bridge"], ecfp_power=9,splif_power=9,flatten=True)
-protein = "protein/6nm8-receptor-preprocessed.pdb"
-ligand_file = ""
+#protein = "data/protein/6NM8-receptor.pdb"
+ligand_file = "data/compounds/2__1000.sdf"
+
+
 def extract_grid_feature(ligand_file):
     feature = featurizer._featurize((ligand_file, protein))
     return feature
 
-#extract_grid_feature(ligand_file = "compounds/2--1000.sdf")
+def extract_plec_feature(ligand_file):
+    receptor = next(oddt.toolkit.readfile("pdb", protein))
+    receptor.protein = True
+    mol = next(oddt.toolkit.readfile('sdf', ligand_file))
+    feature = PLEC(mol, receptor, size = 4092, depth_protein = 4, depth_ligand = 2, distance_cutoff = 4.5, sparse = False)
+    return feature
 
-feature = featurizer._featurize((ligand_file, protein))
+
+grid_feature = extract_grid_feature(ligand_file)
+plec_feature = extract_plec_feature(ligand_file)
+
